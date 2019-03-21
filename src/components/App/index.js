7fc7b90074cff94f571f73f408c6cf2854f29365
vegar <vegar@vi-kan.net>
2019-03-21 09:00:50 +0100

    Firebase in React Setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,14 +9,13 @@ const App = () => (
   <Router>
     <Navigation />
     <hr />
-
-  <Route exact path={ROUTES.LANDING.url} component={Landing} />
-  <Route path={ROUTES.SIGN_UP.url} component={SignUp} />
-  <Route path={ROUTES.SIGN_IN.url} component={SignIn} />
-  <Route path={ROUTES.PASSWORD_FORGET.url} component={PasswordForget} />
-  <Route path={ROUTES.HOME.url} component={Home} />
-  <Route path={ROUTES.ACCOUNT.url} component={Account} />
-  <Route path={ROUTES.ADMIN.url} component={Admin} />
+    <Route exact path={ROUTES.LANDING.url} component={Landing} />
+    <Route path={ROUTES.SIGN_UP.url} component={SignUp} />
+    <Route path={ROUTES.SIGN_IN.url} component={SignIn} />
+    <Route path={ROUTES.PASSWORD_FORGET.url} component={PasswordForget} />
+    <Route path={ROUTES.HOME.url} component={Home} />
+    <Route path={ROUTES.ACCOUNT.url} component={Account} />
+    <Route path={ROUTES.ADMIN.url} component={Admin} />
   </Router>
 );
 
